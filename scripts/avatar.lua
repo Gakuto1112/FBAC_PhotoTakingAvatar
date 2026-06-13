4a1e7bac62b41651480cf9b8fc578379e92c567d
Gakuto1112 <gakuto131112@gmail.com>
2026-06-13 22:20:42 +0900

    コードをわかりやすくリファクタリング

--- scripts/avatar.lua
+++ scripts/avatar.lua
@@ -1,9 +1,10 @@
-BASE_POS = vectors.vec3(238.5, 64, 1276.5)
+---撮影アバターの基準ワールド座標
+BASE_POS = vectors.vec3(0, 0, 0)
 
 ---モデルの親タイプをリセットする。
 ---指定したモデルのサブモデルも対象となる。
 ---@param model ModelPart リセット対象のモデルパーツ
-function resetModelParent(model)
+local function resetModelParent(model)
 	model:setParentType("None")
 	if model:getType() == "GROUP" then
 		if model:getName():match("Halo") then
@@ -15,32 +16,21 @@ function resetModelParent(model)
 	end
 end
 
----クラスをインスタンス化する。
----@generic S
----@generic C
----@param class `C` インスタンス化するクラス
----@param super? `S` インスタンス化するクラスのスーパークラス
----@param ... any クラスのインスタンス時に渡される引数
----@return C instance インスタンス化されたクラスのオブジェクト
----@diagnostic disable-next-line: lowercase-global
-function instantiate(class, super, ...)
-	local instance = super and super.new(...) or {}
-	setmetatable(instance, {__index = class})
-	setmetatable(class, {__index = super})
-	return instance
-end
+---初期化関数
+local function init()
+	local screenSplitLines = require("scripts.screen_split_line")
+	screenSplitLines:init()
 
-require("scripts.screen_split_line")
-local screenSplitLines = ScreenSplitLines.new()
-screenSplitLines:init()
-
-models.models.main.World:setPos(BASE_POS:copy():scale(16))
-for _, modelPart in ipairs(models.models.main.World:getChildren()) do
-	resetModelParent(modelPart)
-end
-events.TICK:register(function ()
+	models.models.main.World:setPos(BASE_POS:copy():scale(16))
 	for _, modelPart in ipairs(models.models.main.World:getChildren()) do
-		local pos = BASE_POS:copy():add(modelPart:getPivot():scale(1 / 16):mul(-1, 1, 1))
-		modelPart:setLight(world.getBlockLightLevel(pos), world.getSkyLightLevel(pos))
+		resetModelParent(modelPart)
 	end
-end)
+	events.TICK:register(function ()
+		for _, modelPart in ipairs(models.models.main.World:getChildren()) do
+			local pos = BASE_POS:copy():add(modelPart:getPivot():scale(1 / 16):mul(-1, 1, 1))
+			modelPart:setLight(world.getBlockLightLevel(pos), world.getSkyLightLevel(pos))
+		end
+	end)
+end
+
+init()
--- scripts/screen_split_line.lua
+++ scripts/screen_split_line.lua
@@ -1,44 +1,36 @@
 ---@class (exact) ScreenSplitLines （撮影のための）画面の分割線を管理するクラス
----@field private X_COUNT integer X軸の画面分割数
----@field private Y_COUNT integer Y軸の画面分割数
-ScreenSplitLines = {}
+---@field package X_COUNT integer X軸の画面分割数
+---@field package Y_COUNT integer Y軸の画面分割数
+local ScreenSplitLines = {
+	X_COUNT = 3;
+	Y_COUNT = 3;
 
----コンストラクタ
----@return ScreenSplitLines
-function ScreenSplitLines.new()
-	---@type ScreenSplitLines
-	local self = instantiate(ScreenSplitLines, nil)
+	---初期化関数
+	---@param self ScreenSplitLines
+	init = function (self)
+		local parent = models:newPart("script_screen_split_lines", "GUI")
+		local screenWidth = client:getScaledWindowSize()
+		for x = 1, self.X_COUNT - 1 do
+			local lineModel = models.models.zone_display.ZoneDisplayBase:copy(client.intUUIDToString(client.generateUUID()))
+			lineModel:setPos(screenWidth.x / self.X_COUNT * x * -1, screenWidth.y * -1, 0)
+			lineModel:setScale(1, screenWidth.y, 1)
+			lineModel:setVisible(true)
+			lineModel:setPrimaryRenderType("EMISSIVE_SOLID")
+			parent:addChild(lineModel)
+		end
+		for y = 1, self.Y_COUNT - 1 do
+			local lineModel = models.models.zone_display.ZoneDisplayBase:copy(client.intUUIDToString(client.generateUUID()))
+			lineModel:setPos(screenWidth.x * -1, screenWidth.y / self.Y_COUNT * y * -1, 0)
+			lineModel:setScale(screenWidth.x, 1, 1)
+			lineModel:setVisible(true)
+			lineModel:setPrimaryRenderType("EMISSIVE_SOLID")
+			parent:addChild(lineModel)
+		end
 
-	self.X_COUNT = 3
-	self.Y_COUNT = 3
-
-	return self
-end
-
----初期化関数
-function ScreenSplitLines:init()
-	local parent = models:newPart("script_screen_split_lines", "GUI")
-	local screenWidth = client:getScaledWindowSize()
-	for x = 1, self.X_COUNT - 1 do
-		local lineModel = models.models.zone_display.ZoneDisplayBase:copy(client.intUUIDToString(client.generateUUID()))
-		lineModel:setPos(screenWidth.x / self.X_COUNT * x * -1, screenWidth.y * -1, 0)
-		lineModel:setScale(1, screenWidth.y, 1)
-		lineModel:setVisible(true)
-		lineModel:setPrimaryRenderType("EMISSIVE_SOLID")
-		parent:addChild(lineModel)
-	end
-	for y = 1, self.Y_COUNT - 1 do
-		local lineModel = models.models.zone_display.ZoneDisplayBase:copy(client.intUUIDToString(client.generateUUID()))
-		lineModel:setPos(screenWidth.x * -1, screenWidth.y / self.Y_COUNT * y * -1, 0)
-		lineModel:setScale(screenWidth.x, 1, 1)
-		lineModel:setVisible(true)
-		lineModel:setPrimaryRenderType("EMISSIVE_SOLID")
-		parent:addChild(lineModel)
-	end
-
-	events.TICK:register(function ()
-		parent:setVisible(client:isHudEnabled())
-	end)
-end
+		events.TICK:register(function ()
+			parent:setVisible(client:isHudEnabled())
+		end)
+	end;
+}
 
 return ScreenSplitLines
